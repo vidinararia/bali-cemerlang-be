7dcf2282380d0179bfb4d7b7c5dc6f9fc52df4bd
vidinararia <vidinararia@gmail.com>
2023-11-29 23:14:52 +0800

    rename tiket pesawat model

--- app/Models/TiketPesawat.php
+++ app/Models/TiketPesawat.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Tiket_pesawat extends Model
+class TiketPesawat extends Model
 {
     use HasFactory;
 
-    protected $table = 'tiket_pesawat';
+    protected $table = 'tiket_pesawats';
     protected $fillable = ['nama_pesawat', 'kota_asal', 'kota_tujuan', 'waktu_berangkat', 'harga'];
 }
