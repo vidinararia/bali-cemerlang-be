fa44c9129a386d56ad65d6f952ee21b3de80d0dd
vidinararia <vidinararia@gmail.com>
2023-11-29 17:02:30 +0800

    create about seeder

---
+++ database/seeders/AboutSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\About;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AboutSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        About::create([
+            'description' => 'test description'
+        ]);
+    }
+}
