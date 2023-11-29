24229c25ecd842f03ab759775f76f65b226b9931
vidinararia <vidinararia@gmail.com>
2023-11-29 17:03:22 +0800

    create controller about

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\About;
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = About::get();
+        return response()->json([
+            'data'=>$data
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request)
+    {
+        $request->validate([
+            'id'=>'required|exists:about,id',
+            'description'=>'required|string',
+        ]);
+
+        $post = About::findOrFail($request->input('id'));
+
+        $post->update([
+            'description'=>$request->input('description'),
+        ]);
+
+        return response()->json([
+            'message'=>'Sukses Update'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
