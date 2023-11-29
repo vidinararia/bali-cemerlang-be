57131d63bc6c524f601a74b4e8279abaaf4753a8
vidinararia <vidinararia@gmail.com>
2023-11-29 23:17:10 +0800

    add offer model

---
+++ database/migrations/2023_11_29_134711_create_offers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul_offer');
+            $table->text('isi_offer');
+            $table->string('jenis_offer');
+            $table->string('foto_offer');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('offers');
+    }
+};
