f04168a4c56c5d52f77bb4700807785f313df111
vidinararia <vidinararia@gmail.com>
2023-12-03 17:54:12 +0800

    update route api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\LokasiController;
 use App\Http\Controllers\OfferController;
+use App\Http\Controllers\PackageController;
 use App\Http\Controllers\TiketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,9 +23,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::get('about', [AboutController::class,'index']);
 Route::put('about', [AboutController::class,'update']);
+
 Route::post('offer', [OfferController::class,'store']);
 Route::get('offer', [OfferController::class,'index']);
 Route::delete('offer', [OfferController::class,'destroy']);
 Route::post('update-offer', [OfferController::class,'update']);
+
+Route::get('package', [PackageController::class,'index']);
+Route::post('package', [PackageController::class,'store']);
+Route::delete('package', [PackageController::class,'destroy']);
+Route::put('package', [PackageController::class,'update']);
