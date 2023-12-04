828a22e89827e3574b45edc67e388b97e928654b
vidinararia <vidinararia@gmail.com>
2023-12-04 15:39:33 +0800

    add softdelete to offer modal

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Offer extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'offers';
     protected $fillable = ['judul_offer', 'isi_offer', 'jenis_offer', 'foto_offer'];
