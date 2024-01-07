322aa7067173e75958a0e8c4211b551d556d67b1
vidinararia <vidinararia@gmail.com>
2024-01-07 14:29:20 +0800

    add auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email'=>'required|string|email',
+            'password'=>'required|string'
+        ]);
+        $credentials = $request->only('email', 'password');
+
+        $token = Auth::attempt($credentials);
+        if (!$token) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+        
+        $authuser = Auth::user();
+
+        return response()->json([
+            'message'=>'Success Login',
+        ]);
+    }
+}
