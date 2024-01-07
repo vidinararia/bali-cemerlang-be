aa5b84ce37f7e788817919b8d3a5bcd9a33da8a2
vidinararia <vidinararia@gmail.com>
2024-01-07 14:22:33 +0800

    update route api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\NewsController;
 use App\Http\Controllers\OfferController;
 use App\Http\Controllers\PackageController;
+use App\Http\Controllers\PromoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,7 +25,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
 Route::controller(AboutController::class)->prefix('about')->group(function () {
     Route::get('', 'index');
     Route::get('view/{id}', 'show');
@@ -32,16 +35,38 @@ Route::controller(OfferController::class)->prefix('offer')->group(function () {
     Route::post('', 'store');
     Route::get('', 'index');
     Route::get('view/{id}', 'show');
-    Route::delete('', 'destroy');
+    Route::delete('{id}', 'destroy');
     Route::post('update', 'update');
-    Route::get('restore', 'restore');
+});
+
+Route::controller(GalleryController::class)->prefix('gallery')->group(function () {
+    Route::post('', 'store');
+    Route::get('', 'index');
+    Route::get('view/{id}', 'show');
+    Route::delete('{id}', 'destroy');
+});
+
+Route::controller(PromoController::class)->prefix('promo')->group(function () {
+    Route::post('', 'store');
+    Route::get('', 'index');
+    Route::get('view/{id}', 'show');
+    Route::delete('{id}', 'destroy');
+    Route::post('update', 'update');
+});
+
+Route::controller(NewsController::class)->prefix('news')->group(function () {
+    Route::post('', 'store');
+    Route::get('', 'index');
+    Route::get('view/{id}', 'show');
+    Route::delete('{id}', 'destroy');
+    Route::post('update', 'update');
 });
 
 Route::controller(PackageController::class)->prefix('package')->group(function () {
     Route::get('', 'index');
     Route::get('view/{id}', 'show');
     Route::post('', 'store');
-    Route::delete('', 'destroy');
+    Route::delete('{id}', 'destroy');
     Route::put('', 'update');
-    Route::get('restore', 'restore');
+    Route::get('restore/{id}', 'restore');
 });
