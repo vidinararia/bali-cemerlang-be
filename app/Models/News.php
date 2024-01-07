7c297fe45c151d04b3b2c381575287de1e0c8a0f
vidinararia <vidinararia@gmail.com>
2024-01-07 14:25:29 +0800

    add news model

---
+++ app/Models/News.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    use HasFactory;
+
+    protected $table = 'news';
+    protected $fillable = ['judul_berita', 'isi_berita', 'foto_berita'];
+}
